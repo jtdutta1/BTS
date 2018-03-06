8a2cf34cfbb069a0b89f9e655d1d462975616ea7
jtdutta1 <jtdutta1@gmail.com>
2018-03-06 23:47:03 +0530

    Some Functional Changes
    The getter view functions have been made accessible to external calls. Also the global variables holding the view images have been commented out to make it faster.

--- image_preprocess.py
+++ image_preprocess.py
@@ -14,36 +14,36 @@ class imagepreprocess():
     # Object accepts the path of the MHA file to be extracted
     def __init__(self,image_array = None, path = None):
         if(image_array == None and not path == None):
-            self.__img = sitk.ReadImage(path)
+            self._img = sitk.ReadImage(path)
         elif(path == None and not image_array == None):
-            self.__img = sitk.GetImageFromArray(image_array)
+            self._img = sitk.GetImageFromArray(image_array)
         else:
             raise Exception('Either a 2D image array has to be provided or a path to the mha file has to be set. Both cannot be None')
-        self.__img_top = self.__getTopView()
-        self.__img_side = self.__getSideView()
-        self.__img_back = self.__getBackView()
+        '''self._img_top = self.__getTopView()
+        self._img_side = self.__getSideView()
+        self._img_back = self.__getBackView()'''
         
-    def __getTopView(self,slices = None):
+    def _getTopView(self,slices = None):
         ''' Returns a 3D numpy array of the top view
         
         slices accepts a list of 2 integers.
         The first number indicates the starting position and the second one indicates the last position.
         '''
         if(slices == None or len(slices) == 0):
-            return sitk.GetArrayFromImage(self.__img)
+            return sitk.GetArrayFromImage(self._img)
         else:
             if(len(slices)>2):
                 raise Exception('Warning!! There are more than 2 elements and we are considering the first 2')
-                return sitk.GetArrayFromImage(self.__img)[slices[0]:slices[1]]
+                return sitk.GetArrayFromImage(self._img)[slices[0]:slices[1]]
             elif(len(slices)<2):
                 raise Exception('slices needs 2 parameters to return the custom MRI scan slices.')
-    def __getSideView(self,slices = None):
+    def _getSideView(self,slices = None):
         ''' slices accepts a list of 2 integers.
             The first number indicates the starting position and the second one indicates the last position.
         '''
         img = []
         for e in range(-239,1):
-            img.append(sitk.GetArrayFromImage(self.__img)[:,:,e])
+            img.append(sitk.GetArrayFromImage(self._img)[:,:,e])
         img = np.array(img)
         if(slices == None or len(slices) == 0):
             return img
@@ -54,13 +54,13 @@ class imagepreprocess():
             elif(len(slices)<2):
                 raise Exception('slices needs 2 parameters to return the custom MRI scan slices.')
     
-    def __getBackView(self,slices = None):
+    def _getBackView(self,slices = None):
         ''' slices accepts a list of 2 integers.
             The first number indicates the starting position and the second one indicates the last position.
         '''
         img = []
         for e in range(-239,1):
-            img.append(sitk.GetArrayFromImage(self.__img)[:,e,:])
+            img.append(sitk.GetArrayFromImage(self._img)[:,e,:])
         img = np.array(img)
         if(slices == None or len(slices) == 0):
             return img
